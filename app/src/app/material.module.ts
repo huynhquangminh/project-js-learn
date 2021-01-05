8776e96fbc19c2a342ebb6c598550910366699f9
DESKTOP-8494VQP\LENOVO <huynhquangminh.cse@gmail.com>
2021-01-05 21:31:27 +0700

    update code layout

--- app/src/app/material.module.ts
+++ app/src/app/material.module.ts
@@ -1,17 +1,20 @@
 import { MatButtonModule } from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
+import {MatRippleModule} from '@angular/material/core';
 import { NgModule } from '@angular/core';
 @NgModule({
     imports: [
         MatButtonModule,
         MatCardModule,
-        MatIconModule
+        MatIconModule,
+        MatRippleModule
     ],
     exports: [
         MatButtonModule,
         MatCardModule,
-        MatIconModule
+        MatIconModule,
+        MatRippleModule
     ]
 })
 export class MaterialModule { }
